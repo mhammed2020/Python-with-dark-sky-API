7aa5bf2158550305a060159f35c3993dc1be1453
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-02 13:53:06 +0100

    fetch data from darksky in the comsole

--- app/views.py
+++ app/views.py
@@ -3,9 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from darksky import forecast
 from datetime import datetime, timedelta
-key = 'dd4652c4b7d73db2e67d5ab119689cc4'
+from datetime import date
+key = 'cc3aae97f4aa5697aa47c5624a085e10'
 def home(request) :
     boston = 42.3601, -71.0589
+    weekday = date.today()
+
+    with forecast('cc3aae97f4aa5697aa47c5624a085e10', *boston) as boston:
+        print("boston daily summary :")
+        for day in boston.daily:
+            day = dict(day = date.strftime(weekday, '%a'),
+                    sum = day.summary,
+                    tempMin = day.temperatureMin,
+                    tempMax = day.temperatureMax
+                    )
+            print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
+            weekday += timedelta(days=1)
 
 
     return render(request,'home.html',{})
