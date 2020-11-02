dfb51e9be41f04e8db193f0db7133fe842b87b58
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-02 13:05:16 +0100

    Start using darksky api with python  in our view

--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-
+from darksky import forecast
+from datetime import datetime, timedelta
+key = 'dd4652c4b7d73db2e67d5ab119689cc4'
 def home(request) :
+    boston = 42.3601, -71.0589
+
 
     return render(request,'home.html',{})
