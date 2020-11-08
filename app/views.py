86daac97c87f046abec30a48fc1619bc1fb72ddb
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-11-08 14:15:04 +0100

    Update views

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ def home(request) :
     # boston = 42.3601, -71.0589
     boston = 34.005143,-6.823645
     weekday = date.today()
-
+    weekly_weather = {}
     with forecast('cc3aae97f4aa5697aa47c5624a085e10', *boston) as boston:
         print(boston.daily.summary)
         for day in boston.daily:
@@ -21,6 +21,24 @@ def home(request) :
             print('{day}: {sum} Temp range: {tempMin} - {tempMax}'.format(**day))
             weekday += timedelta(days=1)
 
+            #------------------------#
+            pic = ''
+            summary = ('{sum}'.format(**day).lower())
+
+            if 'drizzle' in summary :
+                pic ='rain.png'
+            if 'rain' in summary :
+                pic ='rain.png'
+            if 'cloudy' in summary :
+                pic ='clouds.png'
+
+            
+            if 'clear' in summary :
+                pic ='sun.png'
+            if 'party cloudy' in summary :
+                pic ='rain.png'
+            if 'drizzle' in summary :
+                pic ='party-cloudy-day.png'
     hour = datetime.now().hour
     location = forecast('cc3aae97f4aa5697aa47c5624a085e10',34.005143,-6.823645)
     i= 0 
